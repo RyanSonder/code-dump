342cafaf1280ed0123ef686778c4580d7b8c1474
RyanSonder <ryansonderpersonal@gmail.com>
2025-06-26 21:45:37 -0400

    .

---
+++ src/python3/leet_p3340.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# LeetCode Problem 3340 - Check Balanced String
+# 2024-11-09
+
+def isBalanced(self, num: str) -> bool:
+    tmp = 0
+    for char in num:
+        tmp = -(tmp + int(char))
+    return tmp == 0
